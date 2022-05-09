d3b5964b16355491816a16bb0c3d3df9b5f0f5f8
frank9615 <accardifrancesco@hotmail.it>
2022-05-09 14:28:40 +0200

    First Commit

---
+++ src/main/java/com/example/HospitalSpringBoot/HospitalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.HospitalSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HospitalSpringBoot/HospitalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HospitalSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
