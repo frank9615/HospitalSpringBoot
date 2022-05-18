4df2dc745974d8ec61ee45d74328cb5c6bdc4911
frank9615 <accardifrancesco@hotmail.it>
2022-05-18 12:43:09 +0200

    Add InMemoryTest using h2

--- src/test/java/com/example/HospitalSpringBoot/InMemoryDBTest.java
+++ src/test/java/com/example/HospitalSpringBoot/InMemoryDBTest.java
@@ -1,55 +1,43 @@
 package com.example.HospitalSpringBoot;
 
-import com.example.HospitalSpringBoot.entities.Doctor;
+
 import com.example.HospitalSpringBoot.entities.User;
 import com.example.HospitalSpringBoot.enums.Role;
-import com.example.HospitalSpringBoot.repositories.UserRepository;
 import com.example.HospitalSpringBoot.services.IUserService;
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.ContextConfiguration;
-
-import javax.annotation.Resource;
-
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest()
 @ContextConfiguration(classes = HospitalSpringBootApplication.class)
+@Log
 public class InMemoryDBTest {
 
     @Autowired
     private IUserService userService;
 
-    //Add here data inmemory
 
 
     @Test
-    @Order(1)
     public void newUserTest(){
-        User d = new User();
-        d.setName("Test");
-        d.setSurname("Test");
-        d.setUsername("Test");
-        d.setPassword("Test");
-        d.setRole(Role.DOCTOR);
-
-        userService.save(d);
-        User u = userService.getByUsername("Test");
-        assertEquals("Test", u.getUsername());
+        User u = userService.getByUsername("a");
+        assertEquals("a", u.getUsername());
     }
 
     @Test
-    @Order(2)
     public void updateUser(){
-        User u = userService.getByUsername("Test");
-        u.setPassword("Prova");
+        String newPassword = "nuovapassowrd";
+        User u = userService.getByUsername("a");
+        u.setPassword(newPassword);
         userService.save(u);
-        User retrived = userService.getByUsername("Test");
-        assertEquals("Prova", retrived.getPassword());
-
+        User retrived = userService.getByUsername("a");
+        assertEquals(newPassword, retrived.getPassword());
     }
+
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (id, user_type , name, surname, username, password, role) VALUES (1, 'ADMIN', 'a', 'b', 'a', 'a', 'ADMIN' );
+INSERT INTO user (id, user_type , name, surname, username, password, role) VALUES (2, 'DOCTOR', 'b', 'b', 'b', 'b', 'DOCTOR' );
+INSERT INTO user (id, user_type , name, surname, username, password, role) VALUES (3, 'OPERATOR', 'c', 'c', 'c', 'c', 'OPERATOR' );
